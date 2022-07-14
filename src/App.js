054a31cf7c299b3cc95c423bcd1d2608e97887a3
mohanraj-10decoders <mohanraj.l@10decoders.in>
2022-07-14 15:05:06 +0530

    Last commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment, incrementByAmount } from './Redux/employee';
+import { ADD, DELETE } from './Redux/employee';
 
 function App() {
   const { employee } = useSelector((state) => state.employer);
   const dispatch = useDispatch();
   return (
     <div className='App'>
-      <p>Redux app</p>
-      <button onClick={() => dispatch(decrement())}>Dec</button>
-      <span>{employee}</span>
-      <button onClick={() => dispatch(increment())}>Inc</button>
-      <div>
-        <button onClick={() => dispatch(incrementByAmount(10))}>
-          Incre by 10
-        </button>
-      </div>
+      <p>Employee details</p>
+      <button
+        className='addButton'
+        onClick={() =>
+          dispatch(
+            ADD({
+              name: 'new',
+              tech: 'new',
+              mobile: (employee.length + 1) * 1000,
+            })
+          )
+        }
+      >
+        ADD
+      </button>
+      <section className='section'>
+        {employee.map((emp) => {
+          return (
+            <div key={emp.id} className='card'>
+              <p>
+                <strong>Name </strong>
+                <span>{emp.name}</span>
+              </p>
+              <p>
+                <strong>Tech </strong>
+                <span>{emp.tech}</span>
+              </p>
+              <p>
+                <strong>Mobile </strong>
+                <span>{emp.mobile}</span>
+              </p>
+              <button
+                className='deleteButton'
+                onClick={() => dispatch(DELETE({ id: emp.id }))}
+              >
+                DELETE
+              </button>
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
--- src/Redux/employee.js
+++ src/Redux/employee.js
@@ -1,27 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  employee: 0,
+  employee: [
+    {
+      id: 1,
+      name: 'Test',
+      tech: 'Test',
+      mobile: 1234,
+    },
+  ],
 };
 
 export const employerSlice = createSlice({
   name: 'employer',
   initialState,
   reducers: {
-    increment: (state) => {
-      state.employee += 1;
+    ADD: (state, action) => {
+      state.employee = [
+        ...state.employee,
+        {
+          id: state.employee.length + 1,
+          name: action.payload.name,
+          tech: action.payload.tech,
+          mobile: action.payload.mobile,
+        },
+      ];
     },
-    decrement: (state) => {
-      state.employee -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.employee += action.payload;
+    // EDIT: (state, action) => {
+    //   state.employee = state.employee.map((emp) =>
+    //     emp.id == action.payload.id
+    //       ? { ...action.payload.data, id: action.payload.id }
+    //       : emp
+    //   );
+    // },
+    DELETE: (state, action) => {
+      console.log('inside delete', action);
+      state.employee = [
+        ...state.employee.filter((emp) => emp.id != action.payload.id),
+      ];
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } =
-  employerSlice.actions;
+export const { ADD, DELETE } = employerSlice.actions;
 
 export default employerSlice.reducer;
