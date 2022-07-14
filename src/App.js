d769268247abd4a27635bcc2c5c666cc284c0d57
mohanraj-10decoders <mohanraj.l@10decoders.in>
2022-07-14 12:27:38 +0530

    Redux toolkit for counter app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByAmount } from './Redux/counter';
 
 function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className='App'>
       <p>Redux app</p>
+      <button onClick={() => dispatch(decrement())}>Dec</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch(increment())}>Inc</button>
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(10))}>
+          Incre by 10
+        </button>
+      </div>
     </div>
   );
 }
